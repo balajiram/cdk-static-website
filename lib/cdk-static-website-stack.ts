49a1c8ac975aa740e2a307ed3585fa8977bba629
BALAJI <balajikevin@yahoo.com>
2021-10-16 08:24:29 -0700

    bootstrapping the pipeline

--- lib/cdk-static-website-stack.ts
+++ lib/cdk-static-website-stack.ts
@@ -1,9 +1,17 @@
 import * as cdk from '@aws-cdk/core';
+import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
+
 
 export class CdkStaticWebsiteStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'StaticWebsitePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('balajiram/cdk-static-website', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    });
   }
 }
